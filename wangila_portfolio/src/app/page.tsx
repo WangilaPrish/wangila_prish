7fac5f7594bbe76d5a1f111230470bf4b4589d77
WangilaPrish <nabwonyaprish@gmail.com>
2025-08-25 11:38:31 +0300

    feat: reorganize imports in page component and update Montserrat font class for consistency

--- wangila_portfolio/src/app/page.tsx
+++ wangila_portfolio/src/app/page.tsx
@@ -1,11 +1,10 @@
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+import Projects from '@/components/Projects';
+import Technologies from '@/components/Technologies';
 import '@/styles/global.css';
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import React from 'react'
-import Projects from '@/components/Projects'
-import Technologies from '@/components/Technologies'
-import Contact from '@/components/Contact'
 
 const page = () => {
     return (
